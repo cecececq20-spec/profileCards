267dccef31c79938633ef4b1a519644b64fc2e6f
CarnGH <cecececq20@gmail.com>
2026-07-17 14:56:39 +0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,17 @@
+let characters = {
+    profily: { image: "profiles/profily.png", name: "Profily", weight: 70, income: 100 },
+    zombie_profily: { image: "profiles/zorfile.png", name: "Zorfile", weight: 25, income: 5 },
+    ZP: { image: "profiles/ZPN.png", name: "Zatreshka", weight: 1, income: 500 },
+    port : { image: "profiles/port.bmp", name: "Port", weight: 5, income: 100 },
+    RP: { image: "profiles/redprofile.bmp", name: "Redyprof", weight: 25, income: 10 },
+    psyc: { image: "profiles/creppypaint.bmp", name: "Psyc", weight: 25, income: 10 },
+    cheeso: { image: "profiles/cheeso.png", name: "cheeso", weight: 50, income: 5 },
+    chuedusa : { image: "profiles/chuedusa.png", name: "chues-dua", weight: 5, income: 300 },
+    CC : { image: "profiles/crazyclone.png", name: "crazyclone", weight: 25, income: 10 },
+    gvarrdi : { image: "profiles/gvarrdi.png", name: "gvarrdi", weight: 90, income: 1 },
+    milco : { image: "profiles/milco.png", name: "milco", weight: 33, income: 8},
+    MP : { image: "profiles/MP.png", name: "moony", weight: 10, income: 100 },
+    over : { image: "profiles/over.png", name: "over", weight: 70, income: 2 },
+    PP : { image: "profiles/PP.png", name: "painty", weight: 70, income: 1 },
+    stiledProfile : { image: "profiles/stilitro.png", name: "stilitro", weight: 25, income: 10 }
+};
